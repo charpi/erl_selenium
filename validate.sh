19296a040fa374dc8e489267cc96473eedc37df2
charpi <charpi@3ab005ec-33ed-11dd-9e78-a92e4af85449>
2009-01-25 09:44:18 +0000

    Validation with OTP R12-B5

--- validate.sh
+++ validate.sh
@@ -11,8 +11,8 @@ status() {
 }
 
 make start_server 1> /dev/null 2>&1 
-echo -n "Validate with OTP R12B-3 : "
-make ERLDIR=/opt/lib/otp_R12B-3 clean all test 1> /tmp/selenium_validation.log 2>&1 
+echo -n "Validate with OTP R12B-5 : "
+make ERLDIR=/opt/lib/otp_R12B-5 clean all test 1> /tmp/selenium_validation.log 2>&1 
 status $?
 echo -n "Validate with OTP R11B-5 : "
 make ERLDIR=/opt/lib/otp_R11B-5 clean all test  1> /tmp/selenium_validation.log 2>&1 
