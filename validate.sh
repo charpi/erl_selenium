b87d7cdd4bb8e573b02ead36c0224fc7fc31baa0
Nicolas Charpentier <nc@charpi.net>
2010-02-20 11:37:29 +0100

    Remove duplication

--- validate.sh
+++ validate.sh
@@ -1,6 +1,5 @@
 #! /bin/sh 
 
-
 status() {
     case "$1" in
 	"0") echo "Ok"
@@ -10,15 +9,17 @@ status() {
     esac
 }
 
+run_validation() {
+    echo "Validate with $1 : "
+    sed -i -e "s,ERL_TOP.*,ERL_TOP=\"$1\"," erlang_config.rb
+    rake clean 2> /dev/null
+    rake tests 1> /tmp/selenium_validation_R12.log 2>&1 
+    status $?
+}
+
 rake start_server 1> /dev/null 2>&1 
-echo -n "Validate with OTP R12B-5 : "
-sed -i -e "s,ERL_TOP.*,ERL_TOP=\"/usr/local/lib/erlang_R12-B5\"," erlang_config.rb
-rake clean 2> /dev/null
-rake tests 1> /tmp/selenium_validation_R12.log 2>&1 
-status $?
-echo -n "Validate with OTP R13B-0 : "
-sed -i -e "s,ERL_TOP.*,ERL_TOP=\"/usr/local/lib/erlang_R13-B0\"," erlang_config.rb
-rake clean 2> /dev/null
-rake tests  1> /tmp/selenium_validation_R13.log 2>&1 
-status $?
+
+run_validation /usr/local/lib/erlang_R12-B5
+run_validation /usr/local/lib/erlang_R13-B2
+
 rake stop_server 1> /dev/null 2>&1
